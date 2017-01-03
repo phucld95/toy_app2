2b8ed449b673c979a2b7cb29bdb23ad604578f65
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-01-03 10:06:01 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  get 'static_pages/contact'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
-  root 'users#index'
+root 'static_pages#home'
 
 end
